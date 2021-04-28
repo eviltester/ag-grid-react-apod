b9c684a4057affc22648ffaf535fef698f21bc44
Alan Richardson <alan.richardson@ag-grid.com>
2021-04-28 15:10:46 +0100

    pagination
    added more robust fetch, with pagination on the grid

--- src/App.js
+++ src/App.js
@@ -20,31 +20,53 @@ const App = () => {
     setGridColumnApi(params.columnApi);
   }
 
+
+
   const refreshData = e => {
+    document.querySelectorAll('.piccount').forEach((element)=> element.innerText="Loading Images...");
+    
     fetch(
       "https://api.nasa.gov/planetary/apod?api_key=xE8H0ER9bxzelj6850UugbXcs5wi5cgEq1tZcvSv&count=15&thumbs=true"
     )
-      .then(result => result.json())
-      .then(rowData => setRowData(rowData));
+      .then(response => {
+        if(response.ok){
+          return response.json();
+        }else{
+          throw new Error("NASA API Issue");
+        }
+      })
+      .then(data => rowData.concat(data))
+      .then(data => {document.querySelectorAll('.piccount').forEach((element)=> element.innerText="");return data;})
+      .then(rowData => setRowData(rowData))
+      .then(gridApi?.hideOverlay())
+      .catch(error => {console.log(error);document.querySelectorAll('.piccount').forEach((element)=> element.innerText="Loading Error: Try Again");})
+      ;
   };
-  // todo: resize row on image load
-  // todo: store data in a memory model and add to data on 'get new pictures' to support client side pagination
+
+// add loading overlay
 
   return (
     <div>
       <h1>Random Nasa Pictures of the Day</h1>
+      {/*
       <ul>
         <li>Date is sortable</li>
         <li>Date links to apod.nasa.gov page</li>
         <li>Click on Image to see original</li>
+        <li>Filter on explanation</li>
       </ul>
-      <button onClick={refreshData}>Get New Pictures</button>
+      */}
+      <button onClick={refreshData}>Get More Pictures</button><span class="piccount"></span>
       <div className="ag-theme-alpine" style={{ height: 700, width: "100%" }}>
-        <AgGridReact onGridReady={onGridReady} rowData={rowData}>
+        <AgGridReact onGridReady={onGridReady} rowData={rowData}
+          pagination={true}
+          paginationPageSize={10}
+        >
           <AgGridColumn
             field="date"
             width="120"
             sortable={true}
+            resizable={true}
             cellRenderer={function(params) {
               const mydateVals = params.value.split("-");
               var myurl =
@@ -83,6 +105,7 @@ const App = () => {
             field="url"
             flex="1"
             autoHeight={true}
+            headerName="Image"
             cellRenderer={function(params) {
               if (params.node.data.media_type == "image") {
                 return (
@@ -113,6 +136,10 @@ const App = () => {
           />
         </AgGridReact>
       </div>
+      <div style={{"text-align":"right"}} >
+      <span class="piccount"></span><button onClick={refreshData}>Get More Pictures</button>
+      </div>
+
       <p>
         <a href="https://apod.nasa.gov/apod/archivepix.html" target="_blank">
           Visit full archive
