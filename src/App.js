e32105cf76e99e2e71a3d05e1d04c64dc61dbd54
Alan Richardson <alan.richardson@ag-grid.com>
2021-08-26 19:55:40 +0100

    made more reacty

--- src/App.js
+++ src/App.js
@@ -5,69 +5,63 @@ import { AgGridColumn, AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-const VideoImageRenderer = memo(props => {
+const VideoImageRenderer = props => {
 
+  // set image to size with the row when loading
   const[imageStyle, setImageStyle] = useState({height:props.node.rowHeight + 'px', width:'auto'});
+  const[mediaDisplay, setMediaDisplay] = useState();
+  
 
-  const setImageBasedOnRowHeight = ()=>{
-    setImageStyle(({height:props.node.rowHeight + 'px', width:'auto'}));
+  const setImageToFillCell = ()=>{
+    setImageStyle({height:'100%', width:'auto'});
   }
 
-  // if row height changes then amend the image size
-  // TODO: when cell size adjustment added...once a px value has been set, we then want to amend it to height 100% width:auto to let the browser handle it
   useEffect(()=>{
-    setImageBasedOnRowHeight();
-  },[props.node.rowHeight])
+    switch(props.node.data.media_type) {
+      case "image":
+        setMediaDisplay({href: props.value, imgSrc: props.value, mediaType:"image", alt: props.node.data.title, title: undefined});
+        break;
+      case "video":
+        setMediaDisplay({href: props.value, imgSrc: props.node.data.thumbnail_url, mediaType:"video", alt:props.node.data.title, title: "Watch Video Now"});
+        break;
+      default:
+        setMediaDisplay({href: undefined, imgSrc: undefined, mediaType:"other", alt: undefined, title: props.node.data.media_type});
+        break;
+    }
 
-  // TODO: combine these by calculating the variable parts early and adding to state based on props
-  if(props.node.data.media_type=="image"){    
+  },[props.node.data.media_type]);
+
+  if(mediaDisplay===undefined)
+    return null;
+
     return (
-      <a href={props.value} target='_blank'>
-        <img style={imageStyle} src={props.value} loading='lazy' alt={props.node.data.title} onLoad={setImageBasedOnRowHeight}/>
-        </a>  
+
+      <a href={mediaDisplay.href} target='_blank' rel="noreferrer">
+        {mediaDisplay.title && <p>{mediaDisplay.title}</p>}
+        {mediaDisplay.imgSrc &&
+          <img style={imageStyle} src={mediaDisplay.imgSrc} loading='lazy' alt={mediaDisplay.alt} onLoad={setImageToFillCell}/>
+        }
+      </a>  
     )
-  }
 
-  if(props.node.data.media_type=="video"){
-    return(
-      <a href={props.value} target='_blank'>Watch Video Now<br/>
-      <img style={imageStyle} src={props.node.data.thumbnail_url} loading='lazy' alt={props.node.data.title} onLoad={setImageBasedOnRowHeight}/>
-      </a>
-    )
-  }
+};
 
-  return (
-    <a href={props.value} target='_blank'>
-      {props.node.data.media_type}
-    </a>
-  );
-
-});
-
-const DateRenderer= memo(props => {
+const DateRenderer= (props) => {
 
       const mydateVals = props.value.split("-");
       var myurl = "https://apod.nasa.gov/apod/ap" + mydateVals[0].substr(-2, 2) + mydateVals[1] + mydateVals[2] + ".html";
 
       return (
-        <a href={myurl} target='_blank'>{props.value}</a>
+        <a href={myurl} target='_blank' rel='noreferrer'>{props.value}</a>
       );
-});
-
-const TextRenderer= memo(props => {
-    return (
-      <div style={{wordBreak: "normal", lineHeight: "normal"}}><p>
-      {props.value}
-      </p></div>
-    );
-});
+};
 
 
 const App = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
 
-  const [rowData, setRowData] = useState([]);
+  const [rowData, setRowData] = useState(); // defaulting to [] would remove the loading overlay
 
   // load data from api when grid first rendered
   useEffect(() => {
@@ -100,8 +94,8 @@ const App = () => {
             add: data,
           })
         }else{
-          // refresh all data
-          var update= rowData.concat(data);     
+          // refresh all data and handle initial undefined state
+          var update= rowData ? rowData.concat(data) : [].concat(data);     
           setRowData(update);
         }
         })
@@ -119,7 +113,6 @@ const App = () => {
   };
 
 
-  // removed resizable until we make better react implementation to avoid flicker and resize artifacts
   const columnDefs = useMemo(
     () => [
       { field: 'date', width:120, sortable: true, resizable:false, cellRendererFramework: DateRenderer},
@@ -127,11 +120,13 @@ const App = () => {
             autoHeight:true,
             wrapText:true,
             filter:"agTextColumnFilter",
-            resizable:false,
+            resizable:true,
             flex:"2",
-            cellRendererFramework: TextRenderer
+            //cellRendererFramework: TextRenderer
+            // rather than a renderer, just set the styles and let the grid do the work
+            cellStyle: {wordBreak: "normal", lineHeight: "1.5em"}
           },
-      {field: "url", flex:1, headerName:"Image", cellRendererFramework: VideoImageRenderer}
+      {field: "url", flex:1, headerName:"Image", autoHeight:false, cellRendererFramework: VideoImageRenderer}
     ],
     []
   );
@@ -141,7 +136,8 @@ const App = () => {
     <div style={{ height: "100%", width: "100%" }}>
       <button onClick={refreshData}>Get More Pictures</button><span className="piccount"></span>
       <div className="ag-theme-alpine" style={{ height: "100%", width: "100%" }}>
-        <AgGridReact onGridReady={onGridReady} rowData={rowData}
+        <AgGridReact onGridReady={onGridReady}
+         rowData={rowData}
           pagination={true}
           paginationPageSize={10}
           onColumnResized={onColumnResized}
